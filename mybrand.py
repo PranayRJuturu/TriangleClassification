22ee75227e6fe3085fb4b43051849f058ad4f69a
PranayRJuturu <115672420+PranayRJuturu@users.noreply.github.com>
2023-02-12 20:58:58 -0500

    Added mybrand

---
+++ mybrand.py
@@ -0,0 +1,20 @@
+import datetime
+
+def my_brand(assignmentName):
+        
+    now = datetime.datetime.now()
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    print(
+
+        '''
+        =*=*=*= Pranay Reddy Juturu =*=*=*=
+        
+        =*=*=*= Course 2023S-SSW567-A =*=*=*= 
+        
+        '''
+        
+        "=*=*=*= " + assignmentName +" =*=*=*=\n" 
+        
+        "\n\t\t=*=*=*= Date and Time: " + date_time +   " =*=*=*=\n"
+
+    )
